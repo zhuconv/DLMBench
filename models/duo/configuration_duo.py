fb762bd74986d65a2dd555ff26daaf809e3dd6a2
Nandakumar Vignesh (vn5378) <vn5378@austin.utexas.edu@ecen-a52171.austin.utexas.edu>
2025-11-16 15:30:19 -0600

    added duo model

---
+++ models/duo/configuration_duo.py
@@ -0,0 +1,31 @@
+from transformers import AutoConfig, PretrainedConfig
+
+
+class DUOConfig(PretrainedConfig):
+  """Hugging Face configuration class for DUO."""
+  model_type = 'DUO'
+
+  def __init__(
+    self,
+    vocab_size: int = 50258,
+    model_length: int = 1024,
+    causal: bool = False,
+    hidden_dim: int = 768,
+    cond_dim: int = 129,
+    n_blocks: int = 12,
+    n_heads: int = 12,
+    dropout: float = 0.1,
+    var_min: bool = True,
+    ** kwargs):
+    super().__init__(**kwargs)
+    self.causal = causal
+    self.vocab_size = vocab_size
+    self.model_length = model_length
+    self.hidden_dim = hidden_dim
+    self.cond_dim = cond_dim
+    self.n_blocks = n_blocks
+    self.n_heads = n_heads
+    self.dropout = dropout
+    self.var_min = var_min
+
+AutoConfig.register("duo", DUOConfig)
